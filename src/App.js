e633538c0e2755ac88a9775575e79e26362deebe
nikhilgy <nikhilgyanchandani10@gmail.com>
2020-08-18 03:07:03 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import Cards from './components/Cards/Cards';
+
+//import Charts from './components/Charts/Charts';
+
+//import Country from './components/Country/Country';
+
+import {Cards , Charts , Country } from './components';
+import styles from './App.module.css';
+
+import {fetchData} from './api';
+
+
+
+
+class App extends React.Component{
+state = {
+    data : {}
+}
+
+
+
+async componentDidMount(){
+    const fetchedData = await fetchData();
+    this.setState({data : fetchedData})
+}
+
+        render(){
+            const {data} = this.state;
+            return(
+                <div className={styles.container}> 
+                    <Cards data = {data} />
+                    <Country />
+                    <Charts/>
+                </div>
+            )
+        }
+
 }
 
 export default App;
